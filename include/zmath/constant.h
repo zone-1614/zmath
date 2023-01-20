bb0b133c6da8abee9925441787c66ea4e9c9292a
Zone <2493920954@qq.com>
2023-01-20 17:40:34 +0800

    init

---
+++ include/zmath/constant.h
@@ -0,0 +1,9 @@
+
+
+namespace zmath {
+
+const double pi = 3.14159265358979323846;
+
+const double exp = 2.71828182845904523536;
+
+}
---
+++ include/zmath/fft.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <complex>
+#include <valarray>
+
+namespace zmath {
+
+void fft() {
+
+}
+
+void ifft() {
+
+}
+
+}
